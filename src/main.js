8a88ed9f64d6a6489389f7b63ab6ff70c955763b
Veronika Tuzhyk <veronikatuzik@gmail.com>
2026-05-11 13:22:59 +0200

    abc

--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
 import './js/burgerMenu';
 import './css/styles.css';
+
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
